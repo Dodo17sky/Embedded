7b7d795c59bb24f633162fcc7179f24df210f0f3
Home <dodoyln@hotmail.com>
2020-06-06 19:14:31 +0300

    ESP8266\RealProjects\Opportunity
    ESP8266 can react to Uart messages

--- ESP8266/RealProjects/Opportunity/Controller.cpp
+++ ESP8266/RealProjects/Opportunity/Controller.cpp
@@ -1,81 +1,56 @@
 /*--------------------------------------------------------------------------------
 *                           Libraries
 *--------------------------------------------------------------------------------*/
-#include "ComManager.h"
-#include <Arduino.h>
-#include "Serial.h"
 #include "Controller.h"
 
 /*--------------------------------------------------------------------------------
 *                           Global variables
 *--------------------------------------------------------------------------------*/
-static String InMsg_UART;
+
 /*--------------------------------------------------------------------------------
 *                           Const and Macro
 *--------------------------------------------------------------------------------*/
-#define INVALID_MSG_ID				0xFFFF
-#define USER_LED					0x1
+#define OUTPUT_PINS_NUMBER				2U
+
+static const uint8 outputs[OUTPUT_PINS_NUMBER] =
+{
+		D7,
+		D8
+};
 
 /*--------------------------------------------------------------------------------
 *                           Functions prototypes declaration
 *--------------------------------------------------------------------------------*/
-static void Uart_MainFunction(void);
-static void MessageInterpreter(void);
+
 /*--------------------------------------------------------------------------------
 *                           Functions prototypes definition
 *--------------------------------------------------------------------------------*/
 
 /*--------------------------------------------------------------------------------
-@name		ComM_InitRunnable
-@brief		Initialize communication setups
+@name		Controler_InitRunnable
+@brief		Initialize different GPIO pins
 *--------------------------------------------------------------------------------*/
-void ComM_InitRunnable(void)
+void Controller_InitRunnable(void)
 {
-	InMsg_UART.reserve(100);
+	uint8 idx;
+	for(idx=0; idx<OUTPUT_PINS_NUMBER; idx++)
+		pinMode(outputs[idx], OUTPUT);
 }
 
 /*--------------------------------------------------------------------------------
-@name		ComM_MainRunnable
-@brief		Main communication task
+@name		Controler_MainRunnable
+@brief		Main task which controls output pins
 *--------------------------------------------------------------------------------*/
-void ComM_MainRunnable(void)
+void Controller_MainRunnable(void)
 {
-	Uart_MainFunction();
 
-	MessageInterpreter();
 }
 
 /*--------------------------------------------------------------------------------
-@name		Uart_MainFunction
-@brief		Main UART function
+@name		Controller_SetOutput
+@brief		Set the state of a specific pin
 *--------------------------------------------------------------------------------*/
-static void Uart_MainFunction()
+void Controller_SetOutput(uint8 id, uint8 state)
 {
-	InMsg_UART = Serial_Read();
-}
-
-/*--------------------------------------------------------------------------------
-@name		MessageInterpreter
-@brief		Analyze an UART message and
-*--------------------------------------------------------------------------------*/
-static void MessageInterpreter()
-{
-	if(InMsg_UART.startsWith("hey"))
-	{
-		Serial_Write("Hey to you :)\n");
-	}
-	else if(InMsg_UART.startsWith("ledon"))
-	{
-		Controller_SetOutput(USER_LED, HIGH);
-	}
-	else if(InMsg_UART.startsWith("ledoff"))
-	{
-		Controller_SetOutput(USER_LED, LOW);
-	}
-	else if(InMsg_UART != "")
-	{
-		Serial_Write("Reply [");
-		Serial_Write(InMsg_UART);
-		Serial_Write("]\n");
-	}
+	digitalWrite(outputs[id], state);
 }
--- ESP8266/RealProjects/Opportunity/Opportunity.cpp
+++ ESP8266/RealProjects/Opportunity/Opportunity.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
 #include "Serial.h"
 #include "ComManager.h"
+#include "Controller.h"
 
 void setup() {
 	Serial_InitRunnable();
 	ComM_InitRunnable();
+	Controller_InitRunnable();
 }
 
 void loop() {
 	Serial_MainRunnable();
 	ComM_MainRunnable();
+	Controller_MainRunnable();
 }

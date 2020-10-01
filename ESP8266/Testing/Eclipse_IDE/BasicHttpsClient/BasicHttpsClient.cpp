48453e80110f73f478705238580c54b061639916
Home <dodoyln@hotmail.com>
2020-10-01 23:55:10 +0300

    Get temperature from internet

--- ESP8266/Testing/Eclipse_IDE/BasicHttpsClient/BasicHttpsClient.cpp
+++ ESP8266/Testing/Eclipse_IDE/BasicHttpsClient/BasicHttpsClient.cpp
@@ -3,16 +3,21 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
 #define USE_SERIAL Serial
 
 ESP8266WiFiMulti WiFiMulti;
-String GoogleFingerprint = "EA E0 8E 6A AC 0F BA 1C 8C F1 16 E7 41 89 BD 4B 8E 18 27 43";
-String base_Url = "https://script.google.com/macros/s/AKfycbxHYNfpfeJ4k4rW5PuDVML5FzkZOzy5PodMi9lRJ0RT7RQyJsE/exec?";
-String Parameters = "tag=Temp&value=";
-int TempValue = 0;
+String GoogleFingerprint = "EE AA 58 6D 4F 1F 42 F4 18 5B 7F B0 F2 0A 4C DD 97 47 7D 99";
+String Titan_Url = "http://api.openweathermap.org/data/2.5/weather?lat=44.42&lon=26.18&units=metric&APPID=6fe336bc9dcf6fa6f3e35f78551fa62e";
 
 #define SSID        "---"
 #define PASSWORD    "---"
+
+String last_Desc;
+String last_Temp;
+String last_Wind_speed;
+int last_Humi = 0;
 
 void setup() {
     USE_SERIAL.begin(115200);
@@ -29,38 +34,61 @@ void setup() {
 }
 
 void loop() {
-    TempValue++;
-    Parameters = String("tag=Temp&value=") + String(TempValue);
-    String URL = base_Url + Parameters;
-
-    if(TempValue%2 == 0) {
-    	digitalWrite(0, HIGH);
-    }
-    else {
-    	digitalWrite(0, LOW);
-    }
 
     if((WiFiMulti.run() == WL_CONNECTED)) {
 
         HTTPClient http;
 
-        http.begin(URL, GoogleFingerprint);
+        http.begin(Titan_Url);
 
         int httpCode = http.GET();
 
         // httpCode will be negative on error
         if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET status: %d\n", httpCode);
 
             // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.printf("[HTTP] GET data: %s\n", payload.c_str());
-            }
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.printf("[HTTP] GET data: %s\n", payload.c_str());
+            if(httpCode == HTTP_CODE_FOUND || httpCode == HTTP_CODE_OK) {
+            	String payload = http.getString();
+
+            	DynamicJsonDocument doc(1024);
+            	deserializeJson(doc, payload.c_str());
+
+            	const char* desc = doc["weather"][0]["description"];
+            	float temp = doc["main"]["temp"];
+            	float wind_speed = doc["wind"]["speed"];
+            	int humi = doc["main"]["humidity"];
+
+            	String Desc = String(desc);
+            	String Temp = String(temp);
+            	String Wind_speed = String(wind_speed);
+
+            	if (Desc != last_Desc)
+            	{
+            		USE_SERIAL.print("Description: ");
+            		USE_SERIAL.println(Desc);
+            		last_Desc = Desc;
+            	}
+
+            	if (Temp != last_Temp)
+            	{
+					USE_SERIAL.print("Temperature: ");
+					USE_SERIAL.println(Temp);
+					last_Temp = Temp;
+            	}
+
+            	if (Wind_speed != last_Wind_speed)
+				{
+					USE_SERIAL.print("Wind speed: ");
+					USE_SERIAL.println(Wind_speed);
+					last_Wind_speed = Wind_speed;
+				}
+
+            	if (abs(humi-last_Humi) > 0)
+            	{
+					USE_SERIAL.print("Humidity: ");
+					USE_SERIAL.println(humi);
+					last_Humi = humi;
+            	}
             }
 
         } else {
@@ -70,5 +98,5 @@ void loop() {
         http.end();
     }
 
-    delay(4000);
+    delay(20000);
 }
